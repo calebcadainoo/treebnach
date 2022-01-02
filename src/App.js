41da960ef600eb00f0b4972615b1d849edf89212
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-01-02 22:13:47 +0000

    feat: App - app init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          <div onClick={() => alert('Facebook')} className="facebook">
+            Facebook Login
+          </div>
         </p>
         <a
           className="App-link"

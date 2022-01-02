75b1ed679070bf93c0f1357263f5bf0a80edf4e8
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-01-02 22:55:42 +0000

    feat: Facebook Login - implemented facebook login with firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { auth, facebook } from './firebase';
+import { signInWithPopup } from 'firebase/auth';
 
-function App() {
+const App = () => {
+  const FacebookPopUpAuth = () => {
+    console.log('hello');
+    signInWithPopup(auth, facebook)
+      .then((feedback) => {
+        const user = feedback.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = facebook.credentialFromResult(feedback);
+        const accessToken = credential.accessToken;
+
+        console.log('USER: ', user);
+        console.log('CREDENTIAL: ', credential);
+        console.log('TOKEN: ', accessToken);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = facebook.credentialFromError(error);
+
+        console.log('errorCode: ', errorCode);
+        console.log('errorMessage: ', errorMessage);
+        console.log('credential: ', credential);
+        // ...
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          <div onClick={() => alert('Facebook')} className="facebook">
+          <div onClick={() => FacebookPopUpAuth()} className="facebook">
             Facebook Login
           </div>
         </p>
@@ -23,6 +55,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;

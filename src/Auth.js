ee3597f4a51149c435d14c5df6675c61d1d08c5d
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-01-03 01:03:24 +0000

    feat: Facebook Auth - removed default FB SDK code

--- src/Auth.js
+++ src/Auth.js
@@ -1,35 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
 import { auth, facebook } from './firebase';
 import { signInWithPopup } from 'firebase/auth';
 
 const Auth = () => {
-  useEffect(() => {
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: '611632426718211',
-        cookie: true,
-        xfbml: true,
-        version: 'v12.0',
-      });
-
-      window.FB.AppEvents.logPageView();
-    };
-
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  }, []);
-
   const FacebookPopUpAuth = () => {
     console.log('hello');
     signInWithPopup(auth, facebook)

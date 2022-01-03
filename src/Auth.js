aad3ce014854a54a75e77c66d74dd8be71417088
Caleb Crosby Adainoo <code.crosby@gmail.com>
2022-01-03 01:44:31 +0000

    feat: Facebook Auth -added component and state for login and sign out

--- src/Auth.js
+++ src/Auth.js
@@ -1,20 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { auth, facebook } from './firebase';
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const Auth = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [authText, setAuthText] = useState('Click to Login with Facebook Auth');
+
   const FacebookPopUpAuth = () => {
     console.log('hello');
     signInWithPopup(auth, facebook)
       .then((feedback) => {
         const user = feedback.user;
+        setAuthText(user?.displayName);
+        setIsAuth(true);
         console.log('USER: ', user);
       })
       .catch((error) => {
         // Handle Errors here.
         console.log('ERROR: ', error);
         // ...
+      });
+  };
+
+  const Logout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        alert('You have been logged out');
+        setIsAuth(false);
+        setAuthText('Click to Login with Facebook Auth');
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log('SIGN OUT ERROR: ', error);
       });
   };
 
@@ -26,17 +46,12 @@ const Auth = () => {
           <div>
             {/* Edit <code>src/App.js</code> and save to reload. */}
             <div onClick={() => FacebookPopUpAuth()} className="facebook">
-              Facebook Login
+              {authText}
             </div>
           </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <br />
+          <br />
+          <p>{isAuth && <button onClick={Logout}>Log Out</button>}</p>
         </header>
       </div>
     </>
